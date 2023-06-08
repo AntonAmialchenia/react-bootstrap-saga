d410d17ad81550bb1c30f16c83b919b2fe653fa6
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-06-08 12:47:51 +0300

    Создал стор

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./componets/App/";
+
+import { store } from "./store";
 
 import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
