b0dce93d2427590272f4995ef0c6f2f5ba79050d
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-07-14 08:04:07 +0300

    feat: Create post

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Post } from "../types/types";
+import { NewPost, Post } from "../types/types";
 
 export const getPostsApi = async () => {
   return await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
@@ -7,4 +7,10 @@ export const getPostsApi = async () => {
 
 export const deletePostApi = async (id: number) => {
   return await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+};
+
+export const createPostApi = async (post: NewPost) => {
+  return await axios.post<Post>(`https://jsonplaceholder.typicode.com/posts`, {
+    ...post,
+  });
 };
--- src/pages/Posts/Posts.tsx
+++ src/pages/Posts/Posts.tsx
@@ -1,21 +1,44 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import { PostList } from "../../componets/PostList";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { getPosts } from "../../store/slices/postSlice";
+import { getPosts, createPost } from "../../store/slices/postSlice";
 import { SpinnerApp } from "../../componets/SpinnerApp";
+import { ModalApp } from "../../componets/ModalApp";
+
+import { NewPost } from "../../types/types";
+
+import styles from "./Posts.module.scss";
 
 export const Posts: FC = () => {
   const dispatch = useAppDispatch();
-
+  const [show, setShow] = useState<boolean>(false);
   const { items, loading } = useAppSelector((state) => state.posts);
+
+  const postCreate = (newPost: NewPost) => {
+    dispatch(createPost(newPost));
+    setShow((prev) => !prev);
+  };
 
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
 
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center">
+    <div className="d-flex flex-column align-items-center justify-content-center position-relative">
       <h1 className="mb-4">Список постов</h1>
+      <ModalApp
+        show={show}
+        setShow={setShow}
+        create={postCreate}
+        variant="form"
+        title="Новый пост"
+      />
+      <Button
+        onClick={() => setShow((prev) => !prev)}
+        className={`position-fixed rounded-circle ${styles.button}`}>
+        <span>+</span>
+      </Button>
       {loading ? <SpinnerApp /> : <PostList posts={items} />}
     </div>
   );
